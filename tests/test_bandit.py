71c46563b111cb3d3ac2cdf399b136d5c98ea719
NickleDave <nickledave@users.noreply.github.com>
2019-04-16 13:41:39 -0400

    test_bandit.TestMulitArmBandit repeatedly samples from bandit

--- tests/test_bandit.py
+++ tests/test_bandit.py
@@ -11,16 +11,17 @@ class TestMultiArmBandit(unittest.TestCase):
         easy_bandit = MultiArmBandit(p=p_easy)
         arms_0 = np.asarray([0] * 1000000)
         arms_1 = np.asarray([1] * 1000000)
-        r_0 = easy_bandit.pull(arms_0)
-        r_1 = easy_bandit.pull(arms_1)
-        r_pct_0 = np.sum(r_0 == 1) / np.shape(r_0)[0]
-        self.assertTrue(
-            np.allclose(p_easy[0], r_pct_0, atol=1e-3)
-        )
-        r_pct_1 = np.sum(r_1 == 1) / np.shape(r_1)[0]
-        self.assertTrue(
-            np.allclose(p_easy[1], r_pct_1, atol=1e-3)
-        )
+        for _ in range(10):
+            r_0 = easy_bandit.pull(arms_0)
+            r_1 = easy_bandit.pull(arms_1)
+            r_pct_0 = np.sum(r_0 == 1) / np.shape(r_0)[0]
+            self.assertTrue(
+                np.allclose(p_easy[0], r_pct_0, atol=1e-3, rtol=1e-3)
+            )
+            r_pct_1 = np.sum(r_1 == 1) / np.shape(r_1)[0]
+            self.assertTrue(
+                np.allclose(p_easy[1], r_pct_1, atol=1e-3, rtol=1e-3)
+            )
 
 
 if __name__ == '__main__':
