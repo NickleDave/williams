2dbb00b14e120627840db1693b5154a39551fc83
NickleDave <nickledave@users.noreply.github.com>
2019-04-16 17:28:04 -0400

    Agent.__init__ has parameter n_actions, an int, that it uses to make attribute actions_arr, numpy.arange(n_actions)

--- src/williams/agent.py
+++ src/williams/agent.py
@@ -2,8 +2,26 @@ import numpy as np
 
 
 class Agent:
-    def __init__(self, actions, net):
-        self.actions = actions
+    def __init__(self, n_actions, net):
+        """__init__ for Agent class
+
+        Parameters
+        ----------
+        n_actions : int
+            number of actions the agent can take
+        net : class
+            that implements a method 'forward' which accepts inputs with
+            shape (batch size, vector length) and returns outputs of shape
+            (batch size,), where each element in outputs is a member of the
+            set defined by range(self.actions)
+        """
+        if type(n_actions) != int:
+            raise TypeError(f'n_actions must be an integer but got type {type(n_actions)}')
+        if n_actions < 2:
+            raise ValueError(f'value of n_actions must be 2 or greater but got value {n_actions}')
+
+        self.n_actions = n_actions
+        self.actions_arr = np.arange(n_actions)
         self.net = net
 
     def act(self, inputs=None):
