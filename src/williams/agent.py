9611f0154ce563d022ce790ece9a3e0a4e83c233
NickleDave <nickledave@users.noreply.github.com>
2019-04-16 11:30:29 -0400

    add src/williams/agent.by with base Agent class and EpsilonGreedyAgent

---
+++ src/williams/agent.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+
+class Agent:
+    def __init__(self, actions, net):
+        self.actions = actions
+        self.net = net
+
+    def act(self, inputs=None):
+        raise NotImplementedError
+
+
+class EpsilonGreedyAgent(Agent):
+    def __init__(self, actions, net, epsilon):
+        super().__init__(actions, net)
+        self.epsilon = epsilon
+
+    def act(self, inputs=None):
+        xi = np.random.uniform()
+        if xi > self.epsilon:  # act greedily
+            action = self.net.forward(input)
+        else:  # explore
+            action = np.random.choice(self.actions)
+        return action
