feaad7ebffa3a4a8d7e3bc413f751b060d199af9
NickleDave <nickledave@users.noreply.github.com>
2019-04-16 17:52:37 -0400

    changes to trainer.py

--- src/williams/trainer.py
+++ src/williams/trainer.py
@@ -1,5 +1,3 @@
-from time import sleep
-
 from tqdm import tqdm
 import numpy as np
 
@@ -14,32 +12,50 @@ class Trainer:
         self.alpha = alpha
         self.max_reward_p = np.max(bandit.p)
 
-    def train(self, epochs=100, trials=100):
+    def train(self, epochs):
         pbar = tqdm(range(epochs))
         original_w = np.copy(self.agent.w)
+
+        mean_reward_epoch = []
+        p_arm_epoch = []
+        mean_trial_p_epoch = []
+        below_max_epoch = []
+        change_in_w_epoch = []
+
         for epoch in pbar:
-            trial_r = []
-            trial_arms = []
-            trial_p = []
-            for trial in range(trials):
-                arms = self.agent.forward()
-                trial_p.append(self.agent.p)
-                trial_arms.append(arms)
-                r = self.bandit.pull(arms)
-                trial_r.append(r)
-                delta_ws = self.alpha * r * self.agent.e()
-                for delta_w in delta_ws:
-                    self.agent.w += delta_w
-            mean_reward = np.mean(trial_r)
-            uniq_arms, counts = np.unique(trial_arms, return_counts=True)
+            arms = self.agent.act()
+            r = self.bandit.pull(arms)
+            delta_ws = self.alpha * r * self.agent.net.e()
+            for delta_w in delta_ws:
+                self.agent.net.w += delta_w
+
+            mean_reward = np.mean(r)
+            uniq_arms, counts = np.unique(arms, return_counts=True)
             p_arm = np.zeros(shape=(self.bandit.num_arms,))
             for arm_id, count in zip(uniq_arms, counts):
                 p_arm[arm_id] = count / np.sum(counts)
-            mean_trial_p = np.mean(trial_p)
+            mean_trial_p = np.mean(self.agent.net.p)
             below_max = self.max_reward_p - mean_reward
-            change_in_w = np.sum(np.abs(original_w - self.agent.w))
+            change_in_w = np.sum(np.abs(original_w - self.agent.net.w))
+
             pbar.set_description(
                 f'epoch {epoch}, below max:{below_max: 4.3f}, mn trial p:{mean_trial_p: 4.3f}, '
                 f'change w:{change_in_w: 4.3f}, p(arm):{p_arm}, mean reward:{mean_reward: 4.3f}'
             )
-            sleep(0.25)
+
+            mean_reward_epoch.append(mean_reward)
+            p_arm_epoch.append(p_arm)
+            mean_trial_p_epoch.append(mean_trial_p)
+            below_max_epoch.append(below_max)
+            change_in_w_epoch.append(change_in_w)
+
+        summary = {
+            'mean_reward': mean_reward_epoch,
+            'p_arm': p_arm_epoch,
+            'mean_trial_p': mean_trial_p_epoch,
+            'below_max': below_max_epoch
+            'change_in_w': change_in_w_epoch
+        }
+
+        return summary
+
