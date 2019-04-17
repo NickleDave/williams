bfc362e82dfa5785116e11142e90e7f6908255b0
NickleDave <nickledave@users.noreply.github.com>
2019-04-17 09:26:59 -0400

    src/run.py instantiates agent with net=blu (instead of agent being just a blu instance)

--- src/bin/run.py
+++ src/bin/run.py
@@ -1,16 +1,18 @@
 import numpy as np
 
 from williams.units.blu import BernoulliLogisticUnit
+from williams.agent import Agent
 from williams.bandit import MultiArmBandit
 from williams.trainer import Trainer
 
 
 def main():
-    blu = BernoulliLogisticUnit(input_size=20)
+    blu = BernoulliLogisticUnit(input_size=(100, 20))
+    agent = Agent(n_actions=2, net=blu)
     p_easy = np.asarray([0.1, 0.9])
     easy_bandit = MultiArmBandit(p=p_easy)
-    trainer = Trainer(agent=blu, bandit=easy_bandit, alpha=0.01)
-    trainer.train(epochs=10000, trials=100)
+    trainer = Trainer(agent=agent, bandit=easy_bandit, alpha=0.01)
+    trainer.train(epochs=10000)
 
 
 if __name__ == '__main__':
